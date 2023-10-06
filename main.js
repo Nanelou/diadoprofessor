2391ca8efe0e9d1d4dff9e6c1a691a8fe7e1fdc7
NaNeLou <126290968+Nanelou@users.noreply.github.com>
2023-10-06 17:15:05 +0000

    teste

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var mensagem = document.getElementById("mensagem");
+    var mostrarMensagemBtn = document.getElementById("mostrarMensagem");
+
+    mostrarMensagemBtn.addEventListener("click", function () {
+        mensagem.innerHTML = "Obrigado por ser um professor incrível!";
+    });
+});
